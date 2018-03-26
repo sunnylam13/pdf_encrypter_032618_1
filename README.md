ebb2430e82bc044fc52109d7882b71456b444b85
Sunny Lam <sunny.lam@gmail.com>
2018-03-26 18:20:51 -0400

    revised meta

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PDF Mass Encrypter
 
-This program goes through every PDF in a folder and its subfolders and encrypts the PDFs using a password provided on the command line.
+This program goes through every PDF in a folder and its subfolders and encrypts the PDFs using a password provided on the command line.  Also included is the reverse program which decrypts all encrypted pdfs with a provided password.
 
 ## Resources
 
--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ except ImportError:
 	from distutils.core import setup
 
 config = {
-	'description': 'This program goes through every PDF in a folder and its subfolders and encrypts the PDFs using a password provided on the command line.',
+	'description': 'This program goes through every PDF in a folder and its subfolders and encrypts the PDFs using a password provided on the command line.  Also included is the reverse program which decrypts all encrypted pdfs with a provided password.',
 	'author': 'Sunny Lam',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
+	'url': 'https://github.com/sunnylam13/pdf_encrypter_032618_1',
+	'download_url': 'https://github.com/sunnylam13/pdf_encrypter_032618_1',
 	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
