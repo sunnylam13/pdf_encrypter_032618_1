ba9f053042c09fe14416be607046fc58b95c5aac
Sunny Lam <sunny.lam@gmail.com>
2018-03-26 18:34:30 -0400

    revised code

--- pdf_encrypter/pdf_encrypter_032618_1.py
+++ pdf_encrypter/pdf_encrypter_032618_1.py
@@ -3,9 +3,14 @@
 #! python3
 
 # USAGE
-# XX
+# python3 pdf_encrypter_032618_1.py FOLDERPATHSTRING PWDSTRING
+# python3 pdf_encrypter_032618_1.py "../tests/testpdfs1" "yuhelia"
 
-import os, re, shutil, PyPDF2
+import os, re, shutil, PyPDF2, sys
+
+import logging
+logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
+# logging.disable(logging.CRITICAL)
 
 #####################################
 # COMMAND LINE ARGUMENTS AND DATA
@@ -13,8 +18,34 @@ import os, re, shutil, PyPDF2
 
 # get command line arguments and data
 
+user_folderpath = sys.argv[1]
+logging.debug('The target folder is:  %s' % (user_folderpath)  )
+
+user_pwd = sys.argv[2]
+logging.debug('The password to encrypt with is:  %s' % (user_pwd) )
+
 #####################################
 # END COMMAND LINE ARGUMENTS AND DATA
+#####################################
+
+
+#####################################
+# REGEX
+#####################################
+
+# create a regex statement to match `user_file_ext_input`
+# https://regexr.com/3kvi4
+# re.compile should turn a raw string into current regex language so you can skip creating the formula sort of...
+
+user_file_ext_input = '.pdf' # we're targeting only pdf file endings
+# alternatively could use .endswith("pdf") to find it with logic
+
+file_type_regex1 = re.compile(user_file_ext_input + "$")
+# print(file_type_regex1) # for testing
+# print(file_type_regex1.search("testTextA1.txt")) # for testing
+
+#####################################
+# END REGEX
 #####################################
 
 
